9acbe230268c8e6fe36548a5ee57fdf6a6c9374c
ZE0TRON <cmpbilge@gmail.com>
2019-12-08 21:32:39 +0300

    sytnax fix

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -51,5 +51,6 @@ var GoerliBootnodes = []string{
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 // experimental RLPx v5 topic-discovery network.
 var DiscoveryV5Bootnodes = []string{
-"enode://21e167d604bfb5c145c4c2aad1b594742a8f3f801b3e2b0f110791517680ffa0f81c21fa25b5b393d9e84d8a3b59f001cd06580a1aa2f51c2d62c5dac6cc1f47@46.101.114.157:30301",
+	"enode://21e167d604bfb5c145c4c2aad1b594742a8f3f801b3e2b0f110791517680ffa0f81c21fa25b5b393d9e84d8a3b59f001cd06580a1aa2f51c2d62c5dac6cc1f47@46.101.114.157:30301",
 	"enode://21e167d604bfb5c145c4c2aad1b594742a8f3f801b3e2b0f110791517680ffa0f81c21fa25b5b393d9e84d8a3b59f001cd06580a1aa2f51c2d62c5dac6cc1f47@46.101.114.157:0",
+}
