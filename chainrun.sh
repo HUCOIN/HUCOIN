59783097062e989c16c5672c56f6024b7a8138c7
ZE0TRON <cmpbilge@gmail.com>
2020-01-14 17:12:53 +0300

    scripts modified

--- chainrun.sh
+++ chainrun.sh
@@ -1,5 +1,1 @@
-rm -rf ../newcoin_data
-mkdir ../newcoin_data
-./build/bin/geth init --datadir ../newcoin_data/ genesis.json
 ./build/bin/geth  --port 30306 --datadir ../newcoin_data --rpc  --rpcport 8545 --mine --miner.threads=1 --etherbase=0x59641146B8204365f9B0EDBaBE6F09362f02bC3e console
-
